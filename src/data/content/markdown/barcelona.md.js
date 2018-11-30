c03486b08380d2638202b3cda7af0538c4221449
Jack Graham <jackmgraham@hotmail.co.uk>
2018-11-30 17:23:11 +0000

    WIP layouts

--- src/data/content/markdown/barcelona.md.js
+++ src/data/content/markdown/barcelona.md.js
@@ -5,19 +5,26 @@ export default `# Barcelona: Round 3
 
 ### Intro
 
-<section>
-<p>This is a test</p>
-<img class="big" src="${IMG_2730}"></img>
+<section class="big-container">
+<p>
+    Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi nunc magna, dignissim non lacinia at, condimentum et urna. Nullam elementum ante non augue facilisis cursus. Praesent vestibulum, erat nec tincidunt dapibus, elit enim mattis ex, eget fermentum elit turpis nec ligula. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae; Aliquam quis orci et elit aliquam imperdiet quis commodo dui. Donec pretium arcu eu nibh tincidunt consectetur. Vivamus in ipsum et ligula congue commodo quis at leo. Quisque tincidunt est posuere orci egestas, in consectetur erat mattis. In tristique, augue eu fermentum vehicula, felis elit aliquam turpis, eu euismod velit nisl eget arcu. Proin semper tellus urna, sed tempus magna ornare ornare.
+    Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi nunc magna, dignissim non lacinia at, condimentum et urna. Nullam elementum ante non augue facilisis cursus. Praesent vestibulum, erat nec tincidunt dapibus, elit enim mattis ex, eget fermentum elit turpis nec ligula. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae; Aliquam quis orci et elit aliquam imperdiet quis commodo dui. Donec pretium arcu eu nibh tincidunt consectetur. Vivamus in ipsum et ligula congue commodo quis at leo. Quisque tincidunt est posuere orci egestas, in consectetur erat mattis. In tristique, augue eu fermentum vehicula, felis elit aliquam turpis, eu euismod velit nisl eget arcu. Proin semper tellus urna, sed tempus magna ornare ornare.
+    Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi nunc magna, dignissim non lacinia at, condimentum et urna. Nullam elementum ante non augue facilisis cursus. Praesent vestibulum, erat nec tincidunt dapibus, elit enim mattis ex, eget fermentum elit turpis nec ligula. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae; Aliquam quis orci et elit aliquam imperdiet quis commodo dui. Donec pretium arcu eu nibh tincidunt consectetur. Vivamus in ipsum et ligula congue commodo quis at leo. Quisque tincidunt est posuere orci egestas, in consectetur erat mattis. In tristique, augue eu fermentum vehicula, felis elit aliquam turpis, eu euismod velit nisl eget arcu. Proin semper tellus urna, sed tempus magna ornare ornare.
+</p>
+<div class="big-img-container">
+    <img class="big" src="${IMG_2730}"></img>
+</div>
 </section>
 
-This is a test
-
-<img className="test" src="${IMG_2730}"></img>
-
-This is a test
-<img className="test" src="${IMG_2730}"></img>
-This is a test
-
-<img className="test" src="${IMG_2730}"></img>
+<section class="side-container">
+<div class="side-img-container">
+    <img class="side" src="${IMG_2730}"></img>
+</div>
+<p>
+Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi nunc magna, dignissim non lacinia at, condimentum et urna. Nullam elementum ante non augue facilisis cursus. Praesent vestibulum, erat nec tincidunt dapibus, elit enim mattis ex, eget fermentum elit turpis nec ligula. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae; Aliquam quis orci et elit aliquam imperdiet quis commodo dui. Donec pretium arcu eu nibh tincidunt consectetur. Vivamus in ipsum et ligula congue commodo quis at leo. Quisque tincidunt est posuere orci egestas, in consectetur erat mattis. In tristique, augue eu fermentum vehicula, felis elit aliquam turpis, eu euismod velit nisl eget arcu. Proin semper tellus urna, sed tempus magna ornare ornare.
+Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi nunc magna, dignissim non lacinia at, condimentum et urna. Nullam elementum ante non augue facilisis cursus. Praesent vestibulum, erat nec tincidunt dapibus, elit enim mattis ex, eget fermentum elit turpis nec ligula. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae; Aliquam quis orci et elit aliquam imperdiet quis commodo dui. Donec pretium arcu eu nibh tincidunt consectetur. Vivamus in ipsum et ligula congue commodo quis at leo. Quisque tincidunt est posuere orci egestas, in consectetur erat mattis. In tristique, augue eu fermentum vehicula, felis elit aliquam turpis, eu euismod velit nisl eget arcu. Proin semper tellus urna, sed tempus magna ornare ornare.
+Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi nunc magna, dignissim non lacinia at, condimentum et urna. Nullam elementum ante non augue facilisis cursus. Praesent vestibulum, erat nec tincidunt dapibus, elit enim mattis ex, eget fermentum elit turpis nec ligula. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae; Aliquam quis orci et elit aliquam imperdiet quis commodo dui. Donec pretium arcu eu nibh tincidunt consectetur. Vivamus in ipsum et ligula congue commodo quis at leo. Quisque tincidunt est posuere orci egestas, in consectetur erat mattis. In tristique, augue eu fermentum vehicula, felis elit aliquam turpis, eu euismod velit nisl eget arcu. Proin semper tellus urna, sed tempus magna ornare ornare.
+</p>
+</section>
 
 `
